ac5d8d821f8de7e3604affb14e6eb3d90b4efe54
Juansu01 <thejuank0@hotmail.com>
2023-05-25 15:41:34 -0500

    Added new utils module

--- src/routes/main_routes.py
+++ src/routes/main_routes.py
@@ -1,10 +1,11 @@
 from flask import Blueprint
+from utils.openai_prompt import get_completion
 
 bp = Blueprint('routes', __name__)
 
 @bp.route('/')
 def home():
-    return 'Welcome to the home page'
+    return get_completion()
 
 @bp.route('/about')
 def about():
---
+++ src/utils/openai_prompt.py
@@ -0,0 +1,3 @@
+
+def get_completion():
+    return "Excellent!"
