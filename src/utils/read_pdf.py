e3c498a984756b6c8389a49bb2eeb68c4a0904eb
Juansu01 <thejuank0@hotmail.com>
2023-05-25 17:33:02 -0500

    Added function that reads PDF

---
+++ src/utils/read_pdf.py
@@ -0,0 +1,10 @@
+import PyPDF2
+
+def read_pdf(pdf_path):
+    with open(pdf_path, 'rb') as file:
+        pdf_reader = PyPDF2.PdfReader(file)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
